fb70a8fab3f2f82101aba2e2d6ec14fdfb0ad674
Arfat-Adhoni <114485327+Arfat-Adhoni@users.noreply.github.com>
2024-02-03 17:39:10 +0530

    About file changed

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -18,7 +18,7 @@ export function About() {
     <Container id="about">
       <div className="about-text">
         <ScrollAnimation animateIn="fadeInLeft">
-          <h2>About me</h2>
+          <h2>About Me</h2>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
         <p>
