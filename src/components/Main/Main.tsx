197cc9f386315effe8d6e1359c447f78bb402d0f
Arfat Adhoni <114485327+Arfat-Adhoni@users.noreply.github.com>
2024-02-16 16:06:50 +0530

    Update Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -18,6 +18,7 @@ import reactIcon from "../../assets/react-icon.svg";
 import typescriptIcon from "../../assets/typescript-icon.svg";
 import vueIcon from "../../assets/vue-icon.svg";
 import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import { Awards } from "../Awards_Achievements/Awards";
 
 export function Main() {
   return (
@@ -262,6 +263,7 @@ export function Main() {
       <Hero></Hero>
       <About></About>
       <Project></Project>
+      <Awards></Awards>
       <Contact></Contact>
     </Container>
   );
