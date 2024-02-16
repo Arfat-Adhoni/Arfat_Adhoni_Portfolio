fa198b33d1d4bd0c18b5a38f1ca18a044038771d
Arfat Adhoni <114485327+Arfat-Adhoni@users.noreply.github.com>
2024-02-16 16:02:29 +0530

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,6 +7,7 @@ import nodeIcon from "../../assets/node-icon.svg";
 import reactIcon from "../../assets/react-icon.svg";
 import typescriptIcon from "../../assets/typescript-icon.svg";
 import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import Illustration from "../../assets/illustration.svg"
 
 
 
@@ -19,7 +20,7 @@ export function About() {
         </ div>
         < div >
         <p>
-        Arfat Adhoni, a Senior Software Engineer at Mercedes-Benz R&D India, brings over 3 years of experience as a Full-Stack Developer. With an M.Tech degree and expertise in HTML, CSS, JavaScript, React.JS, Java, SpringBoot, and more, he excels in diverse technologies. Proficient in SDLC, cloud platforms, and project management tools, Arfat is adept at handling complex software projects. His passion for innovation and excellence drives him to leverage cutting-edge technologies. Whether you need a new website or a revamp, let's bring your dreams to reality together!
+          Working as Senior Software Engineer at Mercedes-Benz R&D India,Bangalore I bring over 3+ years of experience as a Full-Stack Developer. Holding an M.Tech degree with a university ranking in Design, I am well-versed in a wide array of technologies, including HTML, CSS, JavaScript, JSP, TypeScript, React.JS, Next.JS, Cypress, Java, SpringBoot, Node.JS, Express.JS, Python, FastAPI, MySQL, Vertica, Oracle, CosmoDB, MongoDB, Azure, and Jira. My expertise extends to being an SDLC expert, ensuring a holistic approach to software development. I am passionate about leveraging cutting-edge technologies to drive innovation and excellence in every project I undertake.
          </p>
         </ div>
 
@@ -71,7 +72,7 @@ export function About() {
       </div>
       <div className="about-image">
         < div style={{marginTop : "41px"}}>
-          <img src={ArfatPhoto} style={{height: "320px"}} alt="Arfat" />
+          <img src={Illustration} style={{height: "320px"}} alt="Arfat" />
         </ div>
       </div>
     </Container>
